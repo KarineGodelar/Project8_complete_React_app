314fd2184f71e646846875b17a3066584f401847
Karine <karine.godelar@gmail.com>
2023-07-20 14:21:39 +0200

    wip

---
+++ kasa-react-app/src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import './index.scss';
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="card__picture"></div>
+      <div className="card__title">Titre de la location</div>
+    </div>
+  );
+}
+
+export default Card;
--- kasa-react-app/src/pages/Home/index.jsx
+++ kasa-react-app/src/pages/Home/index.jsx
@@ -1,7 +1,23 @@
 import Banner from '../../components/Banner';
+import Card from '../../components/Card';
+import './index.scss';
 
 function Home() {
-  return <Banner />;
+  return (
+    <div className="home container">
+      <Banner />
+      <div className="cards-container">
+        <Card />
+        <Card />
+        <Card />
+        <div className="cards-container-big-screen">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
