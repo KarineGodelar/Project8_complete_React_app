74b227edc387258ebd3d8704742a07b8fb488efa
Karine <karine.godelar@gmail.com>
2023-07-26 11:01:00 +0200

    wip

--- kasa-react-app/src/components/Card/index.jsx
+++ kasa-react-app/src/components/Card/index.jsx
@@ -1,17 +1,31 @@
 import './index.scss';
 import { Link } from 'react-router-dom';
 
-function Card({ cardUrl, cardTitle, cardPicture }) {
+// cardProps = [id , title, cover]
+function Card({ cardProps }) {
   return (
     <div className="card">
       <div>
-        <img src={cardPicture} alt="appartement" className="card__picture" />
+        <img src={cardProps[2]} alt="appartement" className="card__picture" />
       </div>
-      <Link to={cardUrl} className="card__title">
-        {cardTitle}
+      <Link to={'/fiche-logement/' + cardProps[0]} className="card__title">
+        {cardProps[1]}
       </Link>
     </div>
   );
 }
+
+// function Card({ cardUrl, cardTitle, cardPicture }) {
+//   return (
+//     <div className="card">
+//       <div>
+//         <img src={cardPicture} alt="appartement" className="card__picture" />
+//       </div>
+//       <Link to={cardUrl} className="card__title">
+//         {cardTitle}
+//       </Link>
+//     </div>
+//   );
+// }
 
 export default Card;
--- kasa-react-app/src/pages/Home/index.jsx
+++ kasa-react-app/src/pages/Home/index.jsx
@@ -3,52 +3,21 @@ import Card from '../../components/Card';
 import './index.scss';
 import dataTable from '../../data/kasa';
 
+// cardProps = [id , title, cover]
+
 function Home() {
-  const urlTable = [];
-  const titleTable = [];
-  const pictureTable = [];
-
-  for (let i = 0; i < 6; i++) {
-    urlTable[i] = '/fiche-logement/' + dataTable[i].id;
-    titleTable[i] = dataTable[i].title;
-    pictureTable[i] = dataTable[i].cover;
-  }
-
+  const cardTable = dataTable.map((item) => [item.id, item.title, item.cover]);
   return (
     <div className="home-container">
       <Banner />
       <div className="cards-container">
-        <Card
-          cardUrl={urlTable[0]}
-          cardTitle={titleTable[0]}
-          cardPicture={pictureTable[0]}
-        />
-        <Card
-          cardUrl={urlTable[1]}
-          cardTitle={titleTable[1]}
-          cardPicture={pictureTable[1]}
-        />
-        <Card
-          cardUrl={urlTable[2]}
-          cardTitle={titleTable[2]}
-          cardPicture={pictureTable[2]}
-        />
+        <Card cardProps={cardTable[0]} />
+        <Card cardProps={cardTable[1]} />
+        <Card cardProps={cardTable[2]} />
         <div className="cards-container-big-screen">
-          <Card
-            cardUrl={urlTable[3]}
-            cardTitle={titleTable[3]}
-            cardPicture={pictureTable[3]}
-          />
-          <Card
-            cardUrl={urlTable[4]}
-            cardTitle={titleTable[4]}
-            cardPicture={pictureTable[4]}
-          />
-          <Card
-            cardUrl={urlTable[5]}
-            cardTitle={titleTable[5]}
-            cardPicture={pictureTable[5]}
-          />
+          <Card cardProps={cardTable[3]} />
+          <Card cardProps={cardTable[4]} />
+          <Card cardProps={cardTable[5]} />
         </div>
       </div>
     </div>
